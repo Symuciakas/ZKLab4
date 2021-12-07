7ccaf1a95827e15a373511a1f156256e4312cece
Simas Kazakevičius <simkaz2@ktu.lt>
2021-12-08 00:25:55 +0200

    Should be done

--- ZKLab4/app/src/main/java/com/newproject/zklab4_2/AthleteActivity.java
+++ ZKLab4/app/src/main/java/com/newproject/zklab4_2/AthleteActivity.java
@@ -3,6 +3,7 @@ package com.newproject.zklab4_2;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,15 +15,102 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class AthleteActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
     Context context = this;
     ConstraintLayout athleteActivityLayout;
+    Button button1, button2, button3, button4, button5;
+    BarChart barChart;
     private AlertDialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_athlete);
         athleteActivityLayout = findViewById(R.id.athleteActivityLayout);
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button1.setBackgroundColor(getResources().getColor(R.color.children));
+                button2.setBackgroundColor(getResources().getColor(R.color.main));
+                button3.setBackgroundColor(getResources().getColor(R.color.main));
+                button4.setBackgroundColor(getResources().getColor(R.color.main));
+                button5.setBackgroundColor(getResources().getColor(R.color.main));
+            }
+        });
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button1.setBackgroundColor(getResources().getColor(R.color.main));
+                button2.setBackgroundColor(getResources().getColor(R.color.children));
+                button3.setBackgroundColor(getResources().getColor(R.color.main));
+                button4.setBackgroundColor(getResources().getColor(R.color.main));
+                button5.setBackgroundColor(getResources().getColor(R.color.main));
+            }
+        });
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button1.setBackgroundColor(getResources().getColor(R.color.main));
+                button2.setBackgroundColor(getResources().getColor(R.color.main));
+                button3.setBackgroundColor(getResources().getColor(R.color.children));
+                button4.setBackgroundColor(getResources().getColor(R.color.main));
+                button5.setBackgroundColor(getResources().getColor(R.color.main));
+            }
+        });
+        button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button1.setBackgroundColor(getResources().getColor(R.color.main));
+                button2.setBackgroundColor(getResources().getColor(R.color.main));
+                button3.setBackgroundColor(getResources().getColor(R.color.main));
+                button4.setBackgroundColor(getResources().getColor(R.color.children));
+                button5.setBackgroundColor(getResources().getColor(R.color.main));
+            }
+        });
+        button5 = findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button1.setBackgroundColor(getResources().getColor(R.color.main));
+                button2.setBackgroundColor(getResources().getColor(R.color.main));
+                button3.setBackgroundColor(getResources().getColor(R.color.main));
+                button4.setBackgroundColor(getResources().getColor(R.color.main));
+                button5.setBackgroundColor(getResources().getColor(R.color.children));
+            }
+        });
+        button1.setBackgroundColor(getResources().getColor(R.color.main));
+        button2.setBackgroundColor(getResources().getColor(R.color.main));
+        button3.setBackgroundColor(getResources().getColor(R.color.main));
+        button4.setBackgroundColor(getResources().getColor(R.color.main));
+        button5.setBackgroundColor(getResources().getColor(R.color.main));
+        barChart = findViewById(R.id.barChart);
+        Random random = new Random();
+        ArrayList<BarEntry> data = new ArrayList<>();
+        for(int i = 0; i < 24; i++) {
+            data.add(new BarEntry(i, random.nextInt(30)+20));
+        }
+        BarDataSet barDataSet = new BarDataSet(data, "Data");
+        barDataSet.setColors(ColorTemplate.LIBERTY_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+
+        BarData barData = new BarData(barDataSet);
+        barChart.setFitBars(true);
+        barChart.setData(barData);
+        barChart.getDescription().setText("Some data");
+        barChart.animateY(2000);
 
     }
 
